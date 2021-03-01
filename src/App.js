02d423b3ea0417249bd007545d4cb9b67a06c253
tmjcr <thomascrowdy@gmail.com>
2021-03-01 22:04:51 +0000

    Sensor Trigger hooked up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [data, setdata] = useState([]);
+  const postData = async (e) =>
+    fetch("http://localhost:5000/update", {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: e.target.id,
+        currentStatus: "Triggered",
+        s: "On",
+      }),
+      // body: JSON.stringify({
+      //   name: e.target.id,
+      //   type: "camera",
+      //   currentStatus: "Online",
+      //   range: 12,
+      //   sensitivity: 33,
+      // }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setdata(data);
+        console.log(data);
+      });
+  const postData2 = async (e) =>
+    fetch("http://localhost:5000/update", {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: e.target.id,
+        currentStatus: "Triggered",
+        s: "Off",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setdata(data);
+        console.log(data);
+      });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {" "}
+        SENSORS
+        <div>
+          <button id="Sensor1" onClick={(e) => postData(e)}>
+            Sensor1
+          </button>
+        </div>
+        <div>
+          <button id="Sensor2" onClick={(e) => postData(e)}>
+            Sensor2
+          </button>
+        </div>
+        <div>
+          <button id="Sensor3" onClick={(e) => postData(e)}>
+            Sensor3
+          </button>
+        </div>
+      </div>
+      <div>
+        {" "}
+        Cameras
+        <div>
+          <button id="Camera1" onClick={(e) => postData(e)}>
+            Camera1
+          </button>
+        </div>
+        <div>
+          <button id="Camera2" onClick={(e) => postData(e)}>
+            Camera2
+          </button>
+        </div>
+        <div>
+          <button
+            id="Camera3"
+            onMouseEnter={(e) => postData(e)}
+            onMouseLeave={(e) => postData2(e)}
+          >
+            Camera3
+          </button>
+        </div>
+      </div>
+      <div>
+        {" "}
+        {data.map((item) => (
+          <div>
+            Item: {item.name}
+            <br></br>
+            Status: {item.currentStatus}
+            <div></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
