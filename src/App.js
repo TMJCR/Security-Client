87f8751fb0f6cec5b1331ced791abe66ba326664
tmjcr <thomascrowdy@gmail.com>
2021-03-02 22:54:12 +0000

    PUT request sent on mouse over

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 export default function App() {
   const [data, setdata] = useState([]);
-  const postData = async (e) =>
+  const triggerSensor = async (e, state) =>
     fetch("http://localhost:5000/update", {
       method: "PUT",
       mode: "cors",
@@ -11,33 +11,7 @@ export default function App() {
       },
       body: JSON.stringify({
         name: e.target.id,
-        currentStatus: "Triggered",
-        s: "On",
-      }),
-      // body: JSON.stringify({
-      //   name: e.target.id,
-      //   type: "camera",
-      //   currentStatus: "Online",
-      //   range: 12,
-      //   sensitivity: 33,
-      // }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        setdata(data);
-        console.log(data);
-      });
-  const postData2 = async (e) =>
-    fetch("http://localhost:5000/update", {
-      method: "PUT",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: e.target.id,
-        currentStatus: "Triggered",
-        s: "Off",
+        sensorTrigger: state,
       }),
     })
       .then((response) => response.json())
@@ -48,47 +22,18 @@ export default function App() {
   return (
     <div>
       <div>
-        {" "}
         SENSORS
         <div>
-          <button id="Sensor1" onClick={(e) => postData(e)}>
+          <button
+            id="Sensor1"
+            onMouseOver={(e) => triggerSensor(e, "On")}
+            onMouseLeave={(e) => triggerSensor(e, "Off")}
+          >
             Sensor1
           </button>
         </div>
-        <div>
-          <button id="Sensor2" onClick={(e) => postData(e)}>
-            Sensor2
-          </button>
-        </div>
-        <div>
-          <button id="Sensor3" onClick={(e) => postData(e)}>
-            Sensor3
-          </button>
-        </div>
       </div>
-      <div>
-        {" "}
-        Cameras
-        <div>
-          <button id="Camera1" onClick={(e) => postData(e)}>
-            Camera1
-          </button>
-        </div>
-        <div>
-          <button id="Camera2" onClick={(e) => postData(e)}>
-            Camera2
-          </button>
-        </div>
-        <div>
-          <button
-            id="Camera3"
-            onMouseEnter={(e) => postData(e)}
-            onMouseLeave={(e) => postData2(e)}
-          >
-            Camera3
-          </button>
-        </div>
-      </div>
+
       <div>
         {" "}
         {data.map((item) => (
