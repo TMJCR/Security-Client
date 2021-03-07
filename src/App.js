6a2f3ee0bcc9f033b2a89703f1b78c8b916f8621
tmjcr <thomascrowdy@gmail.com>
2021-03-07 23:40:55 +0000

    POST request added for keypad

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect } from "react";
+import "./style.css";
 export default function App() {
-  const [data, setdata] = useState([]);
+  const [data, setdata] = useState();
+  const [keypadEntry, setKeypadEntry] = useState();
+  useEffect(() => {
+    const fetchData = () => {
+      fetch("http://localhost:5000/", {
+        method: "GET",
+        mode: "cors",
+      })
+        .then((response) => response.json())
+        .then((JSONresponse) => {
+          setdata(JSONresponse);
+        });
+    };
+
+    fetchData();
+  }, []);
+
+  const handleKeypadEntry = () => {
+    setKeypadEntry(keypadEntry);
+  };
+
+  const submitKeypadEntry = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/keypad", {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ enteredCode: 1234 }),
+    })
+      .then((response) => response.json())
+      .then((JSONresponse) => {
+        setdata(JSONresponse);
+      });
+  };
   const triggerSensor = async (e, state) =>
     fetch("http://localhost:5000/update", {
       method: "PUT",
@@ -11,40 +46,76 @@ export default function App() {
       },
       body: JSON.stringify({
         name: e.target.id,
-        sensorTrigger: state,
+        currentState: state,
       }),
     })
       .then((response) => response.json())
-      .then((data) => {
-        setdata(data);
-        console.log(data);
+      .then((JSONresponse) => {
+        setdata(JSONresponse);
       });
   return (
     <div>
       <div>
         SENSORS
         <div>
-          <button
-            id="Sensor1"
-            onMouseOver={(e) => triggerSensor(e, "On")}
-            onMouseLeave={(e) => triggerSensor(e, "Off")}
-          >
-            Sensor1
-          </button>
+          {data &&
+            data.sensors.map((sensor) => (
+              <div key={sensor.status.id}>
+                <div
+                  className="Sensor"
+                  id={sensor.status.name}
+                  onMouseOver={(e) => triggerSensor(e, "On")}
+                  onMouseLeave={(e) => triggerSensor(e, "Off")}
+                >
+                  {sensor.status.name}
+                  <br></br>
+                  {sensor.status.currentStatus}
+                </div>
+              </div>
+            ))}
         </div>
       </div>
-
-      <div>
-        {" "}
-        {data.map((item) => (
-          <div>
-            Item: {item.name}
-            <br></br>
-            Status: {item.currentStatus}
-            <div></div>
+      {data &&
+        data.alarms.map((alarm) => (
+          <div key={alarm.status.id}>
+            <div className="Alarm" id={alarm.status.name}>
+              {alarm.status.name}
+              <br></br>
+              {alarm.status.currentStatus}
+            </div>
           </div>
         ))}
-      </div>
+      {console.log(data)}
+      {data &&
+        data.cameras.map((camera) => (
+          <div key={camera.status.id}>
+            <div className="Camera" id={camera.status.name}>
+              {camera.status.name}
+              <br></br>
+              {camera.status.currentStatus}
+            </div>
+          </div>
+        ))}
+      {data &&
+        data.keypads.map((keypad) => (
+          <div key={keypad.status.id}>
+            <div className="Keypad" id={keypad.status.name}>
+              {keypad.status.name}
+              <form onSubmit={submitKeypadEntry}>
+                <label>
+                  Enter code to reset alarm:
+                  <input
+                    onChange={handleKeypadEntry}
+                    type="text"
+                    name="name"
+                    value={keypadEntry}
+                  />
+                </label>
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+          </div>
+        ))}
     </div>
   );
 }
